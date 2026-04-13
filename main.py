a0c7d95a1261c91b9fa8e340c55a6dee30ffd8ce
yumiykim <ymkim030403@gmail.com>
2026-04-13 18:00:12 +0900

    2026-04-13 개인과제6 3번

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = input("이름을 입력하세요: ")
 student_id = input("학번을 입력하세요: ")
 major = input("학과를 입력하세요: ")
+school = input("학교를 입력하세요: ")
 
 print("\n<출력>")
 print("이름:", name)
 print("학번:", student_id)
 print("학과:", major)
+print("학교:", school)
